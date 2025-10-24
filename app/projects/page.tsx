bc29c4a3d492d48ec1a60fb2499e11cd562d8d7d
Pragati Singh <pragatisngh0417@gmail.com>
2025-10-25 01:01:56 +0530

    updated image

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -61,7 +61,7 @@ export default function Projects() {
   return (
     <main className="bg-white text-gray-800">
       {/* Hero Section */}
-      <section className="relative h-[60vh] md:h-[80vh] sm:h-[60vh] xs:h-[45vh] flex items-center justify-center bg-[url('/images/services-3.JPEG')] bg-cover bg-center">
+      <section className="relative h-[60vh] md:h-[80vh] sm:h-[60vh] xs:h-[45vh] flex items-center justify-center bg-[url('/images/services-3.jpeg')] bg-cover bg-center">
         <div className="absolute inset-0 bg-black/50"></div>
         <motion.div
           initial={{ opacity: 0, y: 40 }}
